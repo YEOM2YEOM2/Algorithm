da5ae9d250b862d19001dcfc0547d0e1193bd298
YEOM2YEOM2 <129479086+YEOM2YEOM2@users.noreply.github.com>
2024-01-10 02:12:18 +0900

    [Silver V] Title: 올림픽, Time: 44 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -9,28 +9,28 @@ for _ in range(N):
     medal.append(temp)
 
 medal.sort(key=lambda x : (x[1], x[2], x[3]), reverse=True)
+medal[0].append(1)
 
-cnt, rank = 1, 1
+cnt = 1
 for i in range(1, N):
     # 현재 금메달 개수가 이전 금메달 개수보다 작으면
     if medal[i-1][1] > medal[i][1]:
-        rank += cnt
+        medal[i].append(medal[i-1][4] + cnt)
         cnt = 1
     # 이미 메달 순으로 정렬해놔서 현재 금메달 개수가 이전 금메달 개수보다 클 일이 없음.
     else:
         # 현재 은메달 개수가 이전 은메달 개수보다 작으면
         if medal[i-1][2] > medal[i][2]:
-            rank += cnt
+            medal[i].append(medal[i - 1][4] + cnt)
             cnt = 1
         else:
             if medal[i-1][3] > medal[i][3]:
-                rank += cnt
+                medal[i].append(medal[i - 1][4] + cnt)
                 cnt = 1
             else:
+                medal[i].append(medal[i-1][4])
                 cnt += 1
 
-    # 등수를 알고 싶은 국가가 나오면 반복문 종료
+for i in range(N):
     if medal[i][0] == K:
-        break;
-
-print(rank)
+        print(medal[i][4])
