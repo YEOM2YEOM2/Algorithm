7072677f03a41e43924870edbec01ae87a248203
YEOM2YEOM2 <129479086+YEOM2YEOM2@users.noreply.github.com>
2024-02-03 14:55:34 +0900

    [Silver IV] Title: 설탕 배달, Time: 128 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+
+        int ans = -1;
+        int temp5 = N / 5;
+        int temp3 = (N - temp5 * 5) / 3;
+        while (temp5 >= 0) {
+            if (N - temp5 * 5 - temp3 * 3 == 0) {
+                ans = temp5 + temp3;
+                break;
+            }
+            temp5 -= 1;
+            temp3 = (N - temp5 * 5) / 3;
+        }
+        System.out.println(ans);
+    }
+}
