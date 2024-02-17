9512b4795e5ed46388dda0e762fc40287e9b35d0
YEOM2YEOM2 <129479086+YEOM2YEOM2@users.noreply.github.com>
2024-02-17 16:27:13 +0900

    [Bronze I] Title: 슈퍼 마리오, Time: 120 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int ans = 0;
+        for (int i = 0; i < 10; i++) {
+            int temp = Integer.parseInt(br.readLine());
+            if (Math.abs(100 - ans - temp) <= Math.abs(100 - ans)) {
+                ans += temp;
+            } else {
+                break;
+            }
+        }
+        System.out.println(ans);
+    }
+}
