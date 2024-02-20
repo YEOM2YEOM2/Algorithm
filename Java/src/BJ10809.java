5f7936a7ce1956b1b7137333aa7b01138093b84e
YEONJI <kkgg54@naver.com>
2024-02-20 18:11:00 +0900

    [Algorithm /Study_Java] BJ1991

---
+++ Java/src/BJ10809.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class BJ10809 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String word = br.readLine();
+        for (int i = 97; i < 123; i++) {
+            System.out.print(word.indexOf((char) i) + " ");
+        }
+    }
+}
