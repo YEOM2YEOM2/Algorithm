bdb995634d7c065089c9b3932f687aabea827911
YEOM2YEOM2 <129479086+YEOM2YEOM2@users.noreply.github.com>
2024-02-23 03:43:05 +0900

    [Gold V] Title: 괄호의 값, Time: 124 ms, Memory: 14420 KB -BaekjoonHub

---
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] bracket = br.readLine().split("");
+
+        Stack<String> stack = new Stack<>();
+        int ans = 0;
+        int temp = 1;
+        for (int i = 0; i < bracket.length; i++) {
+            if (bracket[i].equals("(")) {
+                stack.push("(");
+                temp *= 2;
+            } else if (bracket[i].equals("[")) {
+                stack.push("[");
+                temp *= 3;
+            } else if (bracket[i].equals(")")) {
+                 if (stack.isEmpty() || !stack.peek().equals("(")) { // stack이 비었거나 짝이 맞지 않을 경우 올바르지 않음.
+                     ans = 0;
+                     break;
+                 }
+
+                if (bracket[i - 1].equals("(")) ans += temp;
+                stack.pop();
+                temp /= 2;
+            } else { // ]의 경우
+                if (stack.isEmpty() || !stack.peek().equals("[")) {
+                    ans = 0;
+                    break;
+                }
+
+                if (bracket[i - 1].equals("[")) ans += temp;
+                stack.pop();
+                temp /= 3;
+            }
+        }
+        System.out.println(!stack.isEmpty() ? 0 : ans); // 반복문이 끝났는데 stack이 남아있어도 올바르지 않음.
+    }
+}
