e7e23471b9f7c8dd2585160120ae28c1d03b9324
YEONJI <kkgg54@naver.com>
2024-02-25 18:34:43 +0900

    [Java/study] BJ3584

---
+++ Java/src/study/feb/algo_5th_tree/BJ3584.java
@@ -0,0 +1,42 @@
+package study.feb.algo_5th;
+
+import java.io.*;
+import java.util.*;
+
+public class BJ3584 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 0; t < T; t++) {
+            int N = Integer.parseInt(br.readLine());
+            int E = N - 1;
+            int[] parent = new int[N + 1];
+            for (int i = 0; i < E; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+
+                parent[b] = a;
+            }
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            boolean[] visited = new boolean[N + 1];
+
+            while (a != 0) {
+                visited[a] = true;
+                a = parent[a];
+            }
+
+            while (b != 0) {
+                if (visited[b]) {
+                    System.out.println(b);
+                    break;
+                }
+                b = parent[b];
+            }
+        }
+    }
+}
---
+++ Java/src/study/feb/algo_5th_tree/BJ5052.java
@@ -0,0 +1,72 @@
+package study.feb.algo_5th;
+
+//https://loosie.tistory.com/447
+
+import java.io.*;
+import java.util.*;
+
+public class BJ5052 {
+    static class TrieNode {
+        Map<Character, TrieNode> childNode = new HashMap<>();
+        boolean terminal; // 종료 노드인지 표기하는 값
+
+        public void insert(String word) {
+            TrieNode trie = this;
+            for (int i = 0; i < word.length(); i++) {
+                char c = word.charAt(i);
+                // childNode에 c없으면 추가
+                trie.childNode.putIfAbsent(c, new TrieNode());
+                trie = trie.childNode.get(c);
+                // 마지막 문자 표시
+                if (i == word.length() - 1) {
+                    trie.terminal = true;
+                    return;
+                }
+            }
+        }
+
+        public boolean isContained(String word) {
+            TrieNode trie = this;
+            for (int i = 0; i < word.length(); i++) {
+                char c = word.charAt(i);
+                TrieNode node = trie.childNode.get(c);
+
+                if (node == null) { // key값이 c라는 childNode가 없다면 해당 단어는 Trie에 포함되지 않음.
+                    return false;
+                }
+                trie = node;
+            }
+
+            if (trie.terminal && trie.childNode.isEmpty()) { // 해당 노드가 종료 노드이고 자식 노드가 없다면 자기 자신
+                return false;
+            }
+            return true; // 해당 노드가 종료 노드지만 자식이 있다면 해당 번호는 다른 번호에 포함되는 번호
+        }
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 0; t < T; t++) {
+            int N = Integer.parseInt(br.readLine());
+            TrieNode trie = new TrieNode();
+            boolean isConsistent = true;
+
+            String[] numbers = new String[N];
+            for (int i = 0; i < N; i++) {
+                String temp = br.readLine().strip();
+                trie.insert(temp);
+                numbers[i] = temp;
+            }
+
+            for (String number : numbers) {
+                if (trie.isContained(number)) {
+                    isConsistent = false;
+                    break;
+                }
+            }
+
+            System.out.println(isConsistent ? "YES" : "NO");
+        }
+    }
+}
