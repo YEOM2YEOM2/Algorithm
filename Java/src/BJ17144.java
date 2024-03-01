1cf3522cee0683e43061fad368bac354b2c43dad
YEONJI <kkgg54@naver.com>
2024-03-01 21:16:16 +0900

    [Java/Algorithm] 백준_이진검색트리

---
+++ Java/src/BJ17144.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class BJ17144 {
+    static int R, C, T;
+    static int[][] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        T = Integer.parseInt(st.nextToken());
+
+    }
+}
