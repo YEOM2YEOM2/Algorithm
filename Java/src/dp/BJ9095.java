50f0e50828adef69666c0fc604ea61e59749170c
YEONJI <kkgg54@naver.com>
2024-03-17 18:11:00 +0900

    [Algorithm /Study_Java] 백준 1, 2, 3 더하기

---
+++ Java/src/dp/BJ9095.java
@@ -0,0 +1,21 @@
+package dp;
+
+import java.io.*;
+
+public class BJ9095 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        int[] dp = new int[12];
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i < 11; i++) dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
+
+        for (int t = 0; t < T; t++) {
+            int n = Integer.parseInt(br.readLine());
+            System.out.println(dp[n]);
+        }
+    }
+}
