9a9aaed159d216c46994f9f2d90068342ac82cb7
YEOM2YEOM2 <129479086+YEOM2YEOM2@users.noreply.github.com>
2024-04-07 18:55:11 +0900

    [Silver II] Title: 가희와 키워드, Time: 3476 ms, Memory: 232804 KB -BaekjoonHub

---
+++ 백준/Silver/22233. 가희와 키워드/가희와 키워드.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine().strip());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < N; i++) map.put(br.readLine().strip(), 1);
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine().strip(), ",");
+
+            while (st.hasMoreTokens()) {
+                String key = st.nextToken();
+                if (map.containsKey(key)) map.remove(key);
+            }
+
+            System.out.println(map.size());
+        }
+    }
+}
