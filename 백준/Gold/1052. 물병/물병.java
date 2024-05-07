75811d3c4e82a48e0e951d958790cc243febf2e3
YEOM2YEOM2 <129479086+YEOM2YEOM2@users.noreply.github.com>
2024-05-08 01:46:31 +0900

    [Gold V] Title: 물병, Time: 348 ms, Memory: 16708 KB -BaekjoonHub

---
+++ 백준/Gold/1052. 물병/물병.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine().strip());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] arr = new int[24]; // 2의 제곱 배열
+
+        int temp = 1;
+        for (int i = 0; i < 24; i++) {
+            arr[i] = temp;
+            temp *= 2;
+        }
+
+        int n = N;
+        while (true) {
+            int cnt = 0;
+            for (int i = 0; i < 24; i++) {
+                if ((arr[i] & n) > 0) cnt++;
+            }
+
+            if (cnt <= K) break; // 1의 개수가 K 이하라면 while 문 탈출
+            n++;
+        }
+        System.out.println(n - N);
+    }
+}
