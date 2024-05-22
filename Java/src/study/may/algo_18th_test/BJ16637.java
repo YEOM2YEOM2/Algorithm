e233c19714a15fd07fad0dcfa6092f1e8c768be6
YEONJI <kkgg54@naver.com>
2024-05-22 18:11:00 +0900

    [Java/algorithm] 백준 스타트와 링크

---
+++ Java/src/study/may/algo_18th_test/BJ16637.java
@@ -0,0 +1,11 @@
+package study.may.algo_18th_test;
+
+import java.io.*;
+import java.util.*;
+
+public class BJ16637 {
+
+    public static void main(String[] args) throws IOException {
+
+    }
+}
---
+++ Java/src/study/may/algo_18th_test/BJ17406.java
@@ -0,0 +1,10 @@
+package study.may.algo_18th_test;
+
+import java.io.*;
+import java.util.*;
+public class BJ17406 {
+
+    public static void main(String[] args) throws IOException {
+        
+    }
+}
