5908905ea72c7be5113a5b37cfd29d2dc742aff3
YEOM2YEOM2 <129479086+YEOM2YEOM2@users.noreply.github.com>
2024-05-30 19:16:40 +0900

    [Gold III] Title: 중량제한, Time: 624 ms, Memory: 55940 KB -BaekjoonHub

--- 백준/Gold/1939. 중량제한/중량제한.java
+++ 백준/Gold/1939. 중량제한/중량제한.java
@@ -1,23 +1,20 @@
 import java.io.*;
 import java.util.*;
 
+// 이분 탐색 + DFS
 public class Main {
     static int N, M;
-    static int ans;
-    static Map<Integer, Map<Integer, Integer>> adjL = new HashMap<>(); // key : 출발지, value : (key : 도착지, value : 다리 하중치)
+    static boolean isPossible;
+    static List<Info>[] adjL;
+    static boolean[] visited;
 
-    static class Info implements Comparable<Info> {
+    static class Info {
         int e;
         int d;
 
         public Info(int e, int d) {
             this.e = e;
             this.d = d;
-        }
-
-        @Override
-        public int compareTo(Info o) {
-            return o.d - this.d;
         }
     }
 
@@ -26,59 +23,48 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine().strip());
         N = Integer.parseInt(st.nextToken()); // 섬 개수
         M = Integer.parseInt(st.nextToken()); // 다리 정보의 수
+        adjL = new ArrayList[N + 1];
+        for (int i = 0; i <= N; i++) adjL[i] = new ArrayList<>();
 
+        int left = 0;
+        int right = 0;
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine().strip());
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
             int d = Integer.parseInt(st.nextToken());
 
-            if (adjL.containsKey(s) && adjL.get(s).containsKey(e)) { // 중복된 다리
-                adjL.get(s).put(e, Math.max(adjL.get(s).get(e), d));
-                adjL.get(e).put(s, Math.max(adjL.get(e).get(s), d));
-            } else { // 중복된 다리 x
-                if (!adjL.containsKey(s)) {
-                    if (!adjL.containsKey(e)) { // s, e섬 다리 존재 x
-                        adjL.put(s, new HashMap<>());
-                        adjL.put(e, new HashMap<>());
-                    } else adjL.put(s, new HashMap<>()); // s섬 다리 존재 x, e섬 다리 존재 o
-                } else {
-                    if (!adjL.containsKey(e)) adjL.put(e, new HashMap<>()); // s섬 다리 존재 o, e섬 다리 존재 x
-                }
-                adjL.get(s).put(e, d);
-                adjL.get(e).put(s, d);
-            }
+            adjL[s].add(new Info(e, d));
+            adjL[e].add(new Info(s, d));
+            right = Math.max(right, d);
         }
 
         st = new StringTokenizer(br.readLine().strip());
         int s = Integer.parseInt(st.nextToken());
         int e = Integer.parseInt(st.nextToken());
 
-        dijkstra(s, e);
-        System.out.println(ans);
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            isPossible = false;
+            visited = new boolean[N + 1];
+
+            dfs(s, e, mid);
+            if (isPossible) left = mid + 1;
+            else right = mid - 1;
+        }
+
+        System.out.println(right);
     }
 
-    private static void dijkstra(int s, int e) {
-        PriorityQueue<Info> pq = new PriorityQueue<>();
-        pq.offer(new Info(s, Integer.MAX_VALUE));
-        boolean[] visited = new boolean[N + 1];
+    private static void dfs(int s, int e, int limit) {
+        if (s == e) {
+            isPossible = true;
+            return;
+        }
 
-        while (!pq.isEmpty()) {
-            Info info = pq.poll();
-
-            if (info.e == e) {
-                ans = info.d;
-                return;
-            }
-
-            if (visited[info.e]) continue;
-            visited[info.e] = true;
-
-            for (Map.Entry<Integer, Integer> entry : adjL.get(info.e).entrySet()) {
-                if (!visited[entry.getKey()]) {
-                    pq.offer(new Info(entry.getKey(), Math.min(info.d, entry.getValue())));
-                }
-            }
+        visited[s] = true;
+        for (Info info : adjL[s]) {
+            if (!visited[info.e] && limit <= info.d) dfs(info.e, e, limit);
         }
     }
 }
