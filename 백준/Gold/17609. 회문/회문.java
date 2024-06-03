09bf6632efed6a67cfe4cdfde7faf5e70cbca75e
YEOM2YEOM2 <129479086+YEOM2YEOM2@users.noreply.github.com>
2024-06-03 17:08:32 +0900

    [Gold V] Title: 회문, Time: 348 ms, Memory: 48396 KB -BaekjoonHub

---
+++ 백준/Gold/17609. 회문/회문.java
@@ -0,0 +1,33 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().strip());
+        for (int t = 0; t < T; t++) {
+            String word = br.readLine().strip();
+            StringBuilder sb = new StringBuilder(word);
+            int ans = 2;
+
+            // 회문
+            if (word.equals(sb.reverse().toString())) ans = 0;
+            else {
+                int left = 0;
+                int right = word.length() - 1;
+                while (left < right) {
+                    if (word.charAt(left) != word.charAt(right)) {
+                        StringBuilder leftDel = new StringBuilder(word).deleteCharAt(left);
+                        StringBuilder rightDel = new StringBuilder(word).deleteCharAt(right);
+
+                        if (leftDel.toString().equals(leftDel.reverse().toString()) || rightDel.toString().equals(rightDel.reverse().toString())) ans = 1;
+                        break;
+                    }
+                    left++;
+                    right--;
+                }
+            }
+            System.out.println(ans);
+        }
+    }
+}
