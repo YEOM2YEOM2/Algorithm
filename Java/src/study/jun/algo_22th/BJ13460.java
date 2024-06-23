e7a9a96a075f857ba47da7a6ece8738d35a05de8
YEONJI <kkgg54@naver.com>
2024-06-23 18:11:00 +0900

    [Java/Algorithm] BJ13460 입력

--- Java/src/study/jun/algo_22th/BJ13460.java
+++ Java/src/study/jun/algo_22th/BJ13460.java
@@ -1,4 +1,12 @@
 package study.jun.algo_22th;
 
+import java.io.*;
+import java.util.*;
+
 public class BJ13460 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter br = new BufferedWriter(new InputStreamReader(System.in));
+        
+    }
 }
