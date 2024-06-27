7d77aeec5fb79ef8dd04fcf079dcfe34fecc552f
YEONJI <kkgg54@naver.com>
2024-06-27 18:11:00 +0900

    [Java/Algorithm] BJ13460  풀이 완료

--- Java/src/study/jun/algo_22th/BJ13460.java
+++ Java/src/study/jun/algo_22th/BJ13460.java
@@ -3,6 +3,10 @@ package study.jun.algo_22th;
 import java.io.*;
 import java.util.*;
 
+/*
+- 7% 틀릴 때, visited를 하나의 구슬만 체크, 빨간 구슬 + 파란 구슬 짝으로 방문 체크 해야함.
+- 22% 틀릴 때, 위로 기울이기 잘못됨.
+ */
 public class BJ13460 {
     static int N, M;
     static char[][] arr;
@@ -54,8 +58,8 @@ public class BJ13460 {
     private static int bfs() {
         // 구슬 & 구멍 위치 찾기
         Bead red = new Bead(); Bead blue = new Bead();
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
+        for (int i = 1; i < N - 1; i++) {
+            for (int j = 1; j < M - 1; j++) {
                 if (arr[i][j] != '#') {
                     if (arr[i][j] == 'R') {
                         red = new Bead(i, j);
@@ -69,6 +73,7 @@ public class BJ13460 {
         }
         // bfs 큐 세팅
         Queue<Node> q = new LinkedList<>();
+        HashMap<String, Boolean> visited = new HashMap<>(); // 빨간 구슬 좌표 : key, 파란 구슬 좌표 : value
         q.offer(new Node(red, blue, 0));
         // 기울이기 10회 이하에서 빨간 구슬만 탈출 x
         while (!q.isEmpty()) {
@@ -76,269 +81,117 @@ public class BJ13460 {
 
             if (node.cnt > 10) return -1;
             if (node.blue.i == -1) continue;
-            if (node.red.i == -1 && node.blue.i != -1) return node.cnt;
+            if (node.red.i == -1) return node.cnt;
 
+            String key = "" + node.red.i + node.red.j + node.blue.i + node.blue.j;
+            if (visited.containsKey(key)) continue;
+            visited.put(key, true);
 
-            boolean flag = false;
-            if (node.red.i < node.blue.i) {
-                // 위로 기울이기
-                for (int i = node.red.i - 1; i > -1; i--) {
-                    if (arr[i][node.red.j] == 'O') {
-                        red = new Bead(-1, -1);
-                        flag = true;
-                        break;
-                    } else if (arr[i][node.red.j] == '#') {
-                        red = new Bead(i + 1, node.red.j);
-                        flag = true;
-                        break;
-                    }
+            // 위로 기울이기
+            for (int i = node.red.i - 1; i > -1; i--) {
+                if (arr[i][node.red.j] == 'O') {
+                    red = new Bead(-1, -1);
+                    break;
+                } else if (arr[i][node.red.j] == '#') {
+                    red = new Bead(i + 1, node.red.j);
+                    break;
                 }
-
-                if (!flag) red = new Bead(0, node.red.j);
-
-                flag = false;
-                for (int i = node.blue.i - 1; i > -1; i--) {
-                    if (arr[i][node.blue.j] == 'O') {
-                        blue = new Bead(-1, -1);
-                        flag = true;
-                        break;
-                    } else if (arr[i][node.blue.j] == '#') {
-                        blue = new Bead(i + 1, node.blue.j);
-                        flag = true;
-                        break;
-                    }
-                }
-
-                if (!flag) blue = new Bead(0, node.blue.j);
-                // 파란 공 & 빨간 공 열 위치 같음 + " 행 위치 같음.
-                if (blue.j == red.j && blue.i == red.i) blue.i = red.i + 1;
-                q.offer(new Node(red, blue, node.cnt + 1));
-                // 아래로 기울이기
-                flag = false;
-                for (int i = node.blue.i + 1; i < N; i++) {
-                    if (arr[i][node.blue.j] == 'O') {
-                        blue = new Bead(-1, -1);
-                        flag = true;
-                        break;
-                    } else if (arr[i][node.blue.j] == '#') {
-                        blue = new Bead(i - 1, node.blue.j);
-                        flag = true;
-                        break;
-                    }
-                }
-
-                if (!flag) blue = new Bead(N - 1, node.blue.j);
-
-                flag = false;
-                for (int i = node.red.i; i < N; i++) {
-                    if (arr[i][node.red.j] == 'O') {
-                        red = new Bead(-1, -1);
-                        flag = true;
-                    } else if (arr[i][node.red.j] == '#') {
-                        red = new Bead(i - 1, node.red.j);
-                        flag = true;
-                        break;
-                    }
-                }
-
-                if (!flag) red = new Bead(N - 1, node.red.j);
-                if (red.j == blue.j && red.i == blue.i) red.i = blue.i - 1;
-                q.offer(new Node(red, blue, node.cnt + 1));
-            } else {
-                // 위로 기울이기
-                for (int i = node.blue.i - 1; i > -1; i--) {
-                    if (arr[i][node.blue.j] == 'O') {
-                        blue = new Bead(-1, -1);
-                        flag = true;
-                        break;
-                    } else if (arr[i][node.blue.j] == '#') {
-                        blue = new Bead(i + 1, node.blue.j);
-                        flag = true;
-                        break;
-                    }
-                }
-
-                if (!flag) blue = new Bead(0, node.blue.j);
-
-                flag = false;
-                for (int i = node.red.i - 1; i > -1; i--) {
-                    if (arr[i][node.red.j] == 'O') {
-                        red = new Bead(-1, -1);
-                        flag = true;
-                        break;
-                    } else if (arr[i][node.red.j] == '#') {
-                        red = new Bead(i + 1, node.red.j);
-                        flag = true;
-                        break;
-                    }
-                }
-
-                if (!flag) red = new Bead(0, node.red.j);
-                if (blue.j == red.j && blue.i == red.i) red.i = blue.i + 1;
-                q.offer(new Node(red, blue, node.cnt + 1));
-                // 아래로 기울이기
-                flag = false;
-                for (int i = node.red.i + 1; i < N; i++) {
-                    if (arr[i][node.red.j] == 'O') {
-                        red = new Bead(-1, -1);
-                        flag = true;
-                        break;
-                    } else if (arr[i][node.red.j] == '#') {
-                        red = new Bead(i - 1, node.red.j);
-                        flag = true;
-                        break;
-                    }
-                }
-
-                if (!flag) red = new Bead(N - 1, node.red.j);
-
-                flag = false;
-                for (int i = node.blue.i; i < N; i++) {
-                    if (arr[i][node.blue.j] == 'O') {
-                        blue = new Bead(-1, -1);
-                        flag = true;
-                    } else if (arr[i][node.blue.j] == '#') {
-                        blue = new Bead(i - 1, node.blue.j);
-                        flag = true;
-                        break;
-                    }
-                }
-
-                if (!flag) blue = new Bead(N - 1, node.blue.j);
-                if (red.j == blue.j && red.i == blue.i) blue.i = red.i - 1;
-                q.offer(new Node(red, blue, node.cnt + 1));
             }
-            if (node.red.j > node.blue.j) {
-                // 오른쪽 기울이기
-                flag = false;
-                for (int j = node.red.j + 1; j < M; j++) {
-                    if (arr[node.red.i][j] == 'O') {
-                        red = new Bead(-1, -1);
-                        flag = true;
-                        break;
-                    } else if (arr[node.red.i][j] == '#') {
-                        red = new Bead(node.red.i, j - 1);
-                        flag = true;
-                        break;
-                    }
+
+            for (int i = node.blue.i - 1; i > -1; i--) {
+                if (arr[i][node.blue.j] == 'O') {
+                    blue = new Bead(-1, -1);
+                    break;
+                } else if (arr[i][node.blue.j] == '#') {
+                    blue = new Bead(i + 1, node.blue.j);
+                    break;
                 }
-
-                if (!flag) red = new Bead(node.red.i, M - 1);
-
-                flag = false;
-                for (int j = node.blue.j + 1; j < M; j++) {
-                    if (arr[node.blue.i][j] == 'O') {
-                        blue = new Bead(-1, -1);
-                        flag = true;
-                        break;
-                    } else if (arr[node.blue.i][j] == '#') {
-                        blue = new Bead(node.blue.i, j - 1);
-                        flag = true;
-                        break;
-                    }
-                }
-
-                if (!flag) blue = new Bead(node.blue.i, M - 1);
-                if (red.i == blue.i && red.j == blue.j) blue.j = red.j - 1;
-                q.offer(new Node(red, blue, node.cnt + 1));
-                // 왼쪽 기울이기
-                flag = false;
-                for (int j = node.blue.j - 1; j > -1; j--) {
-                    if (arr[node.blue.i][j] == 'O') {
-                        blue = new Bead(-1, -1);
-                        flag = true;
-                        break;
-                    } else if (arr[node.blue.i][j] == '#') {
-                        blue = new Bead(node.blue.i, j + 1);
-                        flag = true;
-                        break;
-                    }
-                }
-
-                if (!flag) blue = new Bead(node.blue.i, 0);
-
-                flag = false;
-                for (int j = node.red.j - 1; j > -1; j--) {
-                    if (arr[node.red.i][j] == 'O') {
-                        red = new Bead(-1, -1);
-                        flag = true;
-                        break;
-                    } else if (arr[node.red.i][j] == '#') {
-                        red = new Bead(node.red.i, j + 1);
-                        flag = true;
-                        break;
-                    }
-                }
-
-                if (!flag) red = new Bead(node.red.i, 0);
-                if (red.i == blue.i && red.j == blue.j) red.j = blue.j + 1;
-                q.offer(new Node(red, blue, node.cnt + 1));
-            } else {
-                // 오른쪽 기울이기
-                flag = false;
-                for (int j = node.blue.j + 1; j < M; j++) {
-                    if (arr[node.blue.i][j] == 'O') {
-                        blue = new Bead(-1, -1);
-                        flag = true;
-                        break;
-                    } else if (arr[node.blue.i][j] == '#') {
-                        blue = new Bead(node.blue.i, j - 1);
-                        flag = true;
-                        break;
-                    }
-                }
-
-                if (!flag) blue = new Bead(node.blue.i, M - 1);
-
-                flag = false;
-                for (int j = node.red.j + 1; j < M; j++) {
-                    if (arr[node.red.i][j] == 'O') {
-                        red = new Bead(-1, -1);
-                        flag = true;
-                        break;
-                    } else if (arr[node.red.i][j] == '#') {
-                        red = new Bead(node.red.i, j - 1);
-                        flag = true;
-                        break;
-                    }
-                }
-
-                if (!flag) red = new Bead(node.red.i, M - 1);
-                if (red.i == blue.i && red.j == blue.j) red.j = blue.j - 1;
-                q.offer(new Node(red, blue, node.cnt + 1));
-                // 왼쪽 기울이기
-                flag = false;
-                for (int j = node.red.j - 1; j > -1; j--) {
-                    if (arr[node.red.i][j] == 'O') {
-                        red = new Bead(-1, -1);
-                        flag = true;
-                        break;
-                    } else if (arr[node.red.i][j] == '#') {
-                        red = new Bead(node.red.i, j + 1);
-                        flag = true;
-                        break;
-                    }
-                }
-
-                if (!flag) red = new Bead(node.red.i, 0);
-
-                flag = false;
-                for (int j = node.blue.j - 1; j > -1; j--) {
-                    if (arr[node.blue.i][j] == 'O') {
-                        blue = new Bead(-1, -1);
-                        flag = true;
-                        break;
-                    } else if (arr[node.blue.i][j] == '#') {
-                        blue = new Bead(node.blue.i, j + 1);
-                        flag = true;
-                        break;
-                    }
-                }
-
-                if (!flag) blue = new Bead(node.blue.i, 0);
-                if (red.i == blue.i && red.j == blue.j) blue.j = red.j + 1;
-                q.offer(new Node(red, blue, node.cnt + 1));
             }
+
+            // 파란 공 & 빨간 공 열 위치 같음 + " 행 위치 같음.
+            if (red.i != -1 && red.i == blue.i && red.j == blue.j) {
+                if (node.red.i < node.blue.i) blue.i = red.i + 1;
+                else if (node.red.i > node.blue.i) red.i = blue.i + 1;
+            }
+            q.offer(new Node(red, blue, node.cnt + 1));
+            // 아래로 기울이기
+            for (int i = node.red.i + 1; i < N; i++) {
+                if (arr[i][node.red.j] == 'O') {
+                    red = new Bead(-1, -1);
+                    break;
+                } else if (arr[i][node.red.j] == '#') {
+                    red = new Bead(i - 1, node.red.j);
+                    break;
+                }
+            }
+
+            for (int i = node.blue.i + 1; i < N; i++) {
+                if (arr[i][node.blue.j] == 'O') {
+                    blue = new Bead(-1, -1);
+                    break;
+                } else if (arr[i][node.blue.j] == '#') {
+                    blue = new Bead(i - 1, node.blue.j);
+                    break;
+                }
+            }
+
+            if (red.i != -1 && red.i == blue.i && red.j == blue.j) {
+                if (node.red.i < node.blue.i) red.i = blue.i - 1;
+                else if (node.red.i > node.blue.i) blue.i = red.i - 1;
+            }
+            q.offer(new Node(red, blue, node.cnt + 1));
+            // 오른쪽 기울이기
+            for (int j = node.red.j + 1; j < M; j++) {
+                if (arr[node.red.i][j] == 'O') {
+                    red = new Bead(-1, -1);
+                    break;
+                } else if (arr[node.red.i][j] == '#') {
+                    red = new Bead(node.red.i, j - 1);
+                    break;
+                }
+            }
+
+            for (int j = node.blue.j + 1; j < M; j++) {
+                if (arr[node.blue.i][j] == 'O') {
+                    blue = new Bead(-1, -1);
+                    break;
+                } else if (arr[node.blue.i][j] == '#') {
+                    blue = new Bead(node.blue.i, j - 1);
+                    break;
+                }
+            }
+
+            if (red.i != -1 && red.i == blue.i && red.j == blue.j) {
+                if (node.red.j < node.blue.j) red.j = blue.j - 1;
+                else if (node.red.j > node.blue.j) blue.j = red.j - 1;
+            }
+            q.offer(new Node(red, blue, node.cnt + 1));
+            // 왼쪽 기울이기
+            for (int j = node.red.j - 1; j > -1; j--) {
+                if (arr[node.red.i][j] == 'O') {
+                    red = new Bead(-1, -1);
+                    break;
+                } else if (arr[node.red.i][j] == '#') {
+                    red = new Bead(node.red.i, j + 1);
+                    break;
+                }
+            }
+
+            for (int j = node.blue.j - 1; j > -1; j--) {
+                if (arr[node.blue.i][j] == 'O') {
+                    blue = new Bead(-1, -1);
+                    break;
+                } else if (arr[node.blue.i][j] == '#') {
+                    blue = new Bead(node.blue.i, j + 1);
+                    break;
+                }
+            }
+
+            if (red.i != -1 && red.i == blue.i && red.j == blue.j) {
+                if (node.red.j < node.blue.j) blue.j = red.j + 1;
+                else if (node.red.j > node.blue.j) red.j = blue.j + 1;
+            }
+            q.offer(new Node(red, blue, node.cnt + 1));
         }
         return -1;
     }
