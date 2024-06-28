a22f47faa3ae80d61f7b30177a15673d7b89a672
YEONJI <kkgg54@naver.com>
2024-06-28 18:11:00 +0900

    스트릭

---
+++ Java/src/study/jun/algo_22th/BJ17825.java
@@ -0,0 +1,14 @@
+package study.jun.algo_22th;
+
+import java.io.*;
+import java.util.*;
+
+public class BJ17825 {
+    static int ans;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    }
+}
